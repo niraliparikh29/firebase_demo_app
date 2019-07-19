d96bf3309465f5b8c5a0ef02d7626573f12a86d1
niraliparikh29 <44432475+niraliparikh29@users.noreply.github.com>
2019-07-19 19:49:47 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,43 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    //DEMO
+    apiKey: "AIzaSyABLCeu5hVhF2BQwaCGU947g1U-XY8OU9g",
+    authDomain: "fir-6a4db.firebaseapp.com",
+    databaseURL: "https://fir-6a4db.firebaseio.com",
+    projectId: "fir-6a4db",
+    storageBucket: "fir-6a4db.appspot.com",
+    messagingSenderId: "898569706964",
+    appId: "1:898569706964:web:3c324b4ce28399ca"
+
+    // apiKey: "AIzaSyD7DTrkwil-qUnoXtpe5aALXWniZ0DjJXs",
+    // authDomain: "hanglyde-development-76ffd.firebaseapp.com",
+    // databaseURL: "https://hanglyde-development-76ffd.firebaseio.com",
+    // projectId: "hanglyde-development-76ffd",
+    // storageBucket: "hanglyde-development-76ffd.appspot.com",
+    // messagingSenderId: "865565141384",
+    // appId: "1:865565141384:web:19747baf005fc12c"
+
+    //TESTAPP
+    // apiKey: "AIzaSyBsqv8DbfHbJD0OmlqCEfHcKK-WoWtBNOA",
+    // authDomain: "testapp-81333.firebaseapp.com",
+    // databaseURL: "https://testapp-81333.firebaseio.com",
+    // projectId: "testapp-81333",
+    // storageBucket: "testapp-81333.appspot.com",
+    // messagingSenderId: "677633633285",
+    // appId: "1:677633633285:web:001dd357b6826e68"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
